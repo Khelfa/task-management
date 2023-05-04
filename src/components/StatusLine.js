dfea1ca596c8c000ec3310b40f9840be900977cd
Walid <43506524+Khelfa@users.noreply.github.com>
2023-05-04 18:58:03 +0100

    Update StatusLine.js

--- src/components/StatusLine.js
+++ src/components/StatusLine.js
@@ -1,21 +1,27 @@
+// Importing the statusLine.scss file for styling and the Task component
 import "../styles/statusLine.scss";
 import Task from "./Task";
 
+// Defining the StatusLine component as a default export
 export default function StatusLine(props) {
+  // Destructuring the props object to get the required properties
   const { status, tasks, addTask, deleteTask, addEmptyTask, moveTask } = props;
 
   let taskList, tasksForStatus;
 
+  // A function that adds an empty task to the 'tasks' state array with the current status
   function handleAddEmpty() {
     addEmptyTask(status);
   }
 
+  // Filtering the 'tasks' array to get only tasks with the current status
   if (tasks) {
     tasksForStatus = tasks.filter((task) => {
       return task.status === status;
     });
   }
 
+  // Creating an array of Task components for the current status
   if (tasksForStatus) {
     taskList = tasksForStatus.map((task) => {
       return (
@@ -30,10 +36,13 @@ export default function StatusLine(props) {
     });
   }
 
+  // The StatusLine component's render method
   return (
     <div className="statusLine">
       <h3>{status}</h3>
+      {/* Rendering the array of Task components */}
       {taskList}
+      {/* A button that adds an empty task to the 'tasks' state array with the current status */}
       <button onClick={handleAddEmpty} className="button addTask">
         +
       </button>
