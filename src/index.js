59d47330c1b1547408683d1460be9b2946bccf3b
Walid <43506524+Khelfa@users.noreply.github.com>
2023-05-04 19:09:53 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+// Importing React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
+
+// Importing the App component
 import App from "./components/App";
 
+// Rendering the App component inside the root element
 ReactDOM.render(
+  // Enabling strict mode for additional checks
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  // Selecting the root DOM element to render the app in
   document.getElementById("root")
 );
